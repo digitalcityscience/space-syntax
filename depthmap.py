0a722bbc8beb9799bc3f885a85e90f5a609c231a
Dobroslav Totev <dobroslav.totev@hcu-hamburg.de>
2021-10-15 21:29:00 +0200

    feat: auto download  depthmapXcli executable

--- depthmap.py
+++ depthmap.py
@@ -1,5 +1,26 @@
 import asyncio
-from os import path
+from typing import NamedTuple
+from urllib.request import urlretrieve
+from os import chmod, path
+import sys
+
+
+class DepthmapX(NamedTuple):
+    executable: str
+
+
+def depthmapx_factory() -> DepthmapX:
+    if sys.platform == "darwin":
+        executable = "downloads/depthmapX"
+        if path.isfile(executable) is False:
+            urlretrieve(
+                "https://github.com/SpaceGroupUCL/depthmapX/releases/download/v0.8.0/depthmapXcli_macos",
+                executable,
+            )
+        chmod(executable, 0o775)
+        return DepthmapX(executable)
+
+    raise NotImplementedError()
 
 
 async def run(cmd: str, description="Running command"):
@@ -17,42 +38,42 @@ async def run(cmd: str, description="Running command"):
         print(f"[stderr]\n{stderr.decode()}")
 
 
-async def axial(graph_file:str):
+async def axial(graph_file: str, depthmapx:DepthmapX):
     base_file, _ = path.splitext(graph_file)
     axial_map_file = f"{base_file}-axial-map.graph"
     await run(
-        f"depthmapXcli -m MAPCONVERT -f {graph_file} -o {axial_map_file} -p -co axial -con axialMap",
+        f"{depthmapx.executable} -m MAPCONVERT -f {graph_file} -o {axial_map_file} -p -co axial -con axialMap",
         "Converting to axial map",
     )
     axial_analysis_file = f"{base_file}-axial-analysis.graph"
     await run(
-        f"depthmapXcli -m AXIAL -f {axial_map_file} -o {axial_analysis_file} -p -xa 3,n",
+        f"{depthmapx.executable} -m AXIAL -f {axial_map_file} -o {axial_analysis_file} -p -xa 3,n",
         "Performing axial analysis",
     )
     axial_shapefile_mif = f"{base_file}-shapegraph-map.axial.mif"
     await run(
-        f"depthmapXcli -m EXPORT -f {axial_analysis_file} -o {axial_shapefile_mif} -em shapegraph-map-mif",
+        f"{depthmapx.executable} -m EXPORT -f {axial_analysis_file} -o {axial_shapefile_mif} -em shapegraph-map-mif",
         "Exporting axial analysis to mif",
     )
     return axial_shapefile_mif
 
 
-async def segment(graph_file:str):
+async def segment(graph_file: str, depthmapx:DepthmapX):
     base_file, _ = path.splitext(graph_file)
     print("This operation may take longer than 25 minutes to complete!!!")
     segment_map_file = f"{base_file}-segment-map.graph"
     await run(
-        f"depthmapXcli -m MAPCONVERT -f {graph_file} -o {segment_map_file} -p -co segment -con segmentMap",
+        f"{depthmapx.executable} -m MAPCONVERT -f {graph_file} -o {segment_map_file} -p -co segment -con segmentMap",
         "Converting to segment map",
     )
     segment_analysis_file = f"{base_file}-segment-analysis.graph"
     await run(
-        f"depthmapXcli -m SEGMENT -f {segment_map_file} -o {segment_analysis_file} -p  -st metric -sr 200,400,1600",
+        f"{depthmapx.executable} -m SEGMENT -f {segment_map_file} -o {segment_analysis_file} -p  -st metric -sr 200,400,1600",
         "Performing segment analysis",
     )
     segment_shapefile_mif = f"{base_file}-shapegraph-map.segment.mif"
     await run(
-        f"depthmapXcli -m EXPORT -f {segment_analysis_file} -o {segment_shapefile_mif} -em shapegraph-map-mif",
+        f"{depthmapx.executable} -m EXPORT -f {segment_analysis_file} -o {segment_shapefile_mif} -em shapegraph-map-mif",
         "Exporting segment analysis to mif",
     )
     return segment_shapefile_mif
@@ -61,8 +82,9 @@ async def segment(graph_file:str):
 async def analyse(dxfFile: str):
     base_file, _ = path.splitext(dxfFile)
     graph_file = base_file + ".graph"
+    depthmapx = depthmapx_factory()
     await run(
-        f"depthmapXcli -m IMPORT -it drawing -f '{dxfFile}' -o '{graph_file}' ",
+        f"{depthmapx.executable} -m IMPORT -it drawing -f '{dxfFile}' -o '{graph_file}' ",
         "Transform dxf to shape file",
     )
-    return await asyncio.gather(axial(graph_file), segment(graph_file))
+    return await asyncio.gather(axial(graph_file, depthmapx), segment(graph_file, depthmapx))
