8922a8c331dcc7cd9b707975c7fc4d481dd2873e
Dobroslav Totev <dobroslav.totev@hcu-hamburg.de>
2021-10-18 18:13:27 +0200

    feat: add administrative area map

--- download.py
+++ download.py
@@ -1,4 +1,6 @@
-from osmnx import settings, config, graph, io
+import json
+from matplotlib.font_manager import json_load
+from osmnx import settings, config, graph, io, geocode_to_gdf, project_gdf
 import uuid
 
 utn = settings.useful_tags_node
@@ -12,9 +14,12 @@ utw = list(set(utw + oxwa + oxwt))
 config(all_oneway=True, useful_tags_node=utn, useful_tags_way=utw)
 
 
-def download(place: str, operation_id = uuid.uuid4()) -> str:
+def download(place: str, operation_id=uuid.uuid4()) -> str:
     print(f"Downloading map for: {place} ...")
     output_file = f"./downloads/{operation_id}/{operation_id}.osm"
+    city = geocode_to_gdf(place)
     gph = graph.graph_from_place(place, network_type="drive")
     io.save_graph_shapefile(gph, filepath=output_file)
+    with open(f"./downloads/{operation_id}/{operation_id}.administrative.geojson", "w") as fp:
+        json.dump(json.loads(city.to_json()), fp)
     return output_file
