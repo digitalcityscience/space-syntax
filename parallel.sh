34892953a4ce9df81ace85bfeb5304551caeeb84
Dobroslav Totev <dobroslav.totev@hcu-hamburg.de>
2022-02-07 09:05:47 +0100

    feat: switch to detached mode when using docker in parallel mode

--- parallel.sh
+++ parallel.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 TAG=ghcr.io/digitalcityscience/space-syntax
-COMMAND="docker run --network=host --rm -v "${PWD}"/workdir:/home/app/downloads ${TAG}"
+COMMAND="docker run --network=host --detach --rm -v "${PWD}"/workdir:/home/app/downloads ${TAG}"
 
 while IFS= read -r line; do
   echo "Downloading: $line"
   $COMMAND $line &
 done < "$1"
 
-wait 
-echo "All done"
+echo "All triggered"
